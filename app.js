f632c7ef068df7ff847053dc6952f7fe060f0758
MERATWALPRIYANKA3 <69080015+MERATWALPRIYANKA3@users.noreply.github.com>
2021-07-08 10:35:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -34,8 +34,8 @@ app.post('/login', routes.login );
 app.post('/logout', routes.logout );
 
 app.post('/inboundmsg',function(req,res){
-  console.log(req);
-  console.log("Reply Body:"+res.Body);
+  console.log(req.body);
+  console.log("Reply Body:"+res);
 });
 
 // Custom Hello World Activity Routes
